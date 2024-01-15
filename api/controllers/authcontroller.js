5ab0f63eee8d78c28f6fe94d7447e2a4957cde5a
vinaybh01 <vinaybh0107@gmail.com>
2024-01-16 01:20:22 +0530

    fixed signin and signup

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -5,6 +5,16 @@ import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
+
+  if (!email || !username || !password)
+    return next(errorHandler(400, "Please Enter Every Detail"));
+
+  const validUsername = await User.findOne({ username });
+  if (validUsername) return next(errorHandler(400, "Username Already exist"));
+
+  const validEmail = await User.findOne({ email });
+  if (validEmail) return next(errorHandler(400, "Email Already exist"));
+
   const hashedPassword = await bcrypt.hash(password, 10);
   try {
     const newUser = new User({ username, email, password: hashedPassword });
@@ -19,15 +29,16 @@ export const signin = async (req, res, next) => {
   const { email, password } = req.body;
   try {
     const validUser = await User.findOne({ email });
-    if (!validUser) {
-      return next(errorHandler(404, "User Not Found"));
-    }
-    const validPassword = bcrypt.compare(password, validUser.password);
-    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+    if (!validUser) return next(errorHandler(400, "User not found!"));
+
+    const validPassword = await bcrypt.compare(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong credentials!"));
+
     const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
     const { password: pass, ...rest } = validUser._doc;
+
     res
-      .cookie("acces_token", token, { expiresIn: "1h" })
+      .cookie("access_token", token, { httpOnly: true })
       .status(200)
       .json(rest);
   } catch (error) {
--- client/src/pages/SignIn.jsx
+++ client/src/pages/SignIn.jsx
@@ -1,22 +1,27 @@
 import { useState } from "react";
 import { Link, useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
+import {
+  signInStart,
+  signInSuccess,
+  signInFailure,
+} from "../redux/user/userslice.js";
 
-function SignIn() {
+export default function SignIn() {
   const [formData, setFormData] = useState({});
-  const [error, setError] = useState(null);
-  const [loading, setLoading] = useState(false);
+  const { loading, error } = useSelector((state) => state.user);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
   const handleChange = (e) => {
     setFormData({
       ...formData,
       [e.target.id]: e.target.value,
     });
   };
-
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      setLoading(true);
+      dispatch(signInStart());
       const res = await fetch("/api/auth/signin", {
         method: "POST",
         headers: {
@@ -27,19 +32,15 @@ function SignIn() {
       const data = await res.json();
       console.log(data);
       if (data.success === false) {
-        setLoading(false);
-        setError(data.message);
+        dispatch(signInFailure(data.message));
         return;
       }
-      setLoading(false);
-      setError(null);
+      dispatch(signInSuccess(data));
       navigate("/");
     } catch (error) {
-      setLoading(false);
-      setError(error.message);
+      dispatch(signInFailure(error.message));
     }
   };
-
   return (
     <div className="p-3 max-w-lg mx-auto">
       <h1 className="text-3xl text-center font-semibold my-7">Sign In</h1>
@@ -58,6 +59,7 @@ function SignIn() {
           id="password"
           onChange={handleChange}
         />
+
         <button
           disabled={loading}
           className="bg-slate-700 text-white p-3 rounded-lg uppercase hover:opacity-95 disabled:opacity-80"
@@ -66,14 +68,12 @@ function SignIn() {
         </button>
       </form>
       <div className="flex gap-2 mt-5">
-        <p>dont have an account?</p>
-        <Link to={"/sign-in"}>
-          <span className="text-blue-700">Sign Up</span>
+        <p>Dont have an account?</p>
+        <Link to={"/signup"}>
+          <span className="text-blue-700">Sign up</span>
         </Link>
       </div>
       {error && <p className="text-red-500 mt-5">{error}</p>}
     </div>
   );
 }
-
-export default SignIn;
