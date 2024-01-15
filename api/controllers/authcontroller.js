26b3cc9fc055f9742be9f2e0ab8bda3091428cdf
vinaybh01 <vinaybh0107@gmail.com>
2024-01-15 20:48:03 +0530

    create signin api route

--- api/controllers/authcontroller.js
+++ api/controllers/authcontroller.js
@@ -1,5 +1,7 @@
 import bcrypt from "bcrypt";
 import User from "../models/UserModel.js";
+import jwt from "jsonwebtoken";
+import { errorHandler } from "../utils/error.js";
 
 export const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
@@ -8,6 +10,26 @@ export const signup = async (req, res, next) => {
     const newUser = new User({ username, email, password: hashedPassword });
     await newUser.save();
     res.status(201).json("User Created Successfully");
+  } catch (error) {
+    next(error);
+  }
+};
+
+export const signin = async (req, res, next) => {
+  const { email, password } = req.body;
+  try {
+    const validUser = await User.findOne({ email });
+    if (!validUser) {
+      return next(errorHandler(404, "User Not Found"));
+    }
+    const validPassword = bcrypt.compare(password, validUser.password);
+    if (!validPassword) return next(errorHandler(401, "Wrong Credentials"));
+    const token = jwt.sign({ id: validUser._id }, process.env.JWT_SECRET);
+    const { password: pass, ...rest } = validUser._doc;
+    res
+      .cookie("acces_token", token, { expiresIn: "1h" })
+      .status(200)
+      .json(rest);
   } catch (error) {
     next(error);
   }
