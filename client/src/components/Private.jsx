710c1cc7dc3ac899b80975ae78b20a492065d360
vinaybh01 <vinaybh0107@gmail.com>
2024-01-17 03:01:41 +0530

    update profile ui

--- client/src/components/Private.jsx
+++ client/src/components/Private.jsx
@@ -1,13 +1,9 @@
 import { useSelector } from "react-redux";
-import { Navigate } from "react-router-dom";
+import { Navigate, Outlet } from "react-router-dom";
 
 function Private() {
   const { currentUser } = useSelector((state) => state.user);
-  return currentUser ? (
-    <Navigate to={"/profile"} />
-  ) : (
-    <Navigate to={"/signin"} />
-  );
+  return currentUser ? <Outlet /> : <Navigate to={"/signin"} />;
 }
 
 export default Private;
