b33100fd2d1721843ffb1beb2859408579bd7091
vinaybh01 <vinaybh0107@gmail.com>
2024-01-22 03:10:01 +0530

    Create Listing api

--- api/index.js
+++ api/index.js
@@ -3,6 +3,7 @@ import mongoose from "mongoose";
 import dotenv from "dotenv";
 import userRouter from "./routes/userRoute.js";
 import authRouter from "./routes/authRoute.js";
+import listingRouter from "./routes/listingRoute.js";
 import cors from "cors";
 import cookieParser from "cookie-parser";
 const app = express();
@@ -30,6 +31,7 @@ app.listen(3000, () => {
 
 app.use("/api/user", userRouter);
 app.use("/api/auth", authRouter);
+app.use("/api/listing", listingRouter);
 
 app.use((err, req, res, next) => {
   const statusCode = err.statusCode || 500;
