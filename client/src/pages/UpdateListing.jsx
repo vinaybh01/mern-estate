803f3b11f23483134a68dcab898485abb6c15ec9
vinaybh01 <vinaybh0107@gmail.com>
2024-01-29 02:28:12 +0530

    complete contact landload functionality

--- client/src/pages/UpdateListing.jsx
+++ client/src/pages/UpdateListing.jsx
@@ -306,7 +306,9 @@ export default function UpdateListing() {
               />
               <div className="flex flex-col items-center">
                 <p>Regular price</p>
-                <span className="text-xs">($ / month)</span>
+                {formData.type === "rent" && (
+                  <span className="text-xs">($ / month)</span>
+                )}{" "}
               </div>
             </div>
             {formData.offer && (
@@ -323,7 +325,9 @@ export default function UpdateListing() {
                 />
                 <div className="flex flex-col items-center">
                   <p>Discounted price</p>
-                  <span className="text-xs">($ / month)</span>
+                  {formData.type === "rent" && (
+                    <span className="text-xs">($ / month)</span>
+                  )}{" "}
                 </div>
               </div>
             )}
