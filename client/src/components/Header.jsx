6453a16eacbbbba12df109cc6b24db1ddc0b89a9
vinaybh01 <vinaybh0107@gmail.com>
2024-02-04 12:57:19 +0530

    complete about page

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -29,8 +29,8 @@ function Header() {
       <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
         <Link to="/">
           <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
-            <span className="text-slate-500">Sahand</span>
-            <span className="text-slate-700">Estate</span>
+            <span className="text-slate-500">Elite</span>
+            <span className="text-slate-700">EstatesHub</span>
           </h1>
         </Link>
         <form
