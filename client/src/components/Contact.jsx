f725c0907f40df7bc974ff92c2395d775b8b06d3
vinaybh01 <vinaybh0107@gmail.com>
2024-02-18 22:57:52 +0530

    fix contact

--- client/src/components/Contact.jsx
+++ client/src/components/Contact.jsx
@@ -48,12 +48,12 @@ export default function Contact({ listing }) {
             className="w-full border p-3 rounded-lg"
           ></textarea>
 
-          <Link
-            to={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
+          <a
+            href={`mailto:${landlord.email}?subject=Regarding ${listing.name}&body=${message}`}
             className="bg-slate-700 text-white text-center p-3 uppercase rounded-lg hover:opacity-95"
           >
             Send Message
-          </Link>
+          </a>
         </div>
       )}
     </>
