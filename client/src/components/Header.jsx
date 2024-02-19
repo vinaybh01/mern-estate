c1a44c3cffbf5704dfe18e729598032f154e9a47
vinaybh01 <vinaybh0107@gmail.com>
2024-02-19 15:11:04 +0530

    fixed header

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -25,15 +25,21 @@ function Header() {
   }, [location.search]);
 
   return (
-    <header className="bg-slate-200 shadow-md">
-      <div className="flex justify-between items-center max-w-6xl mx-auto p-3">
+    <header className="bg-[#FFFFFF] shadow-md h-14">
+      <div className="flex justify-between items-center mx-12">
         <Link to="/">
-          <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
+          {/* <h1 className="font-bold text-sm sm:text-xl flex flex-wrap">
             <span className="text-slate-500">Elite</span>
             <span className="text-slate-700">EstatesHub</span>
-          </h1>
+          </h1> */}
+          <img
+            src="https://www.longandfoster.com/images/Uploads/RECos/35009/ContentFiles/lf-logo.png"
+            alt=""
+            width={"200px"}
+            height={"200px"}
+          />
         </Link>
-        <form
+        {/* <form
           onSubmit={handleSubmit}
           className="bg-slate-100 p-3 rounded-lg flex items-center"
         >
@@ -47,17 +53,16 @@ function Header() {
           <button>
             <FaSearch className="text-slate-600" />
           </button>
-        </form>
-        <ul className="flex gap-4">
+        </form> */}
+        <ul className="flex gap-8 font-semibold text-black max-w-6xl p-3">
           <Link to="/">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              Home
-            </li>
+            <li className="hidden sm:inline hover:underline">HOME</li>
           </Link>
           <Link to="/about">
-            <li className="hidden sm:inline text-slate-700 hover:underline">
-              About
-            </li>
+            <li className="hidden sm:inline  hover:underline ">ABOUT</li>
+          </Link>
+          <Link to="/search">
+            <li className="hidden sm:inline  hover:underline ">SEARCH</li>
           </Link>
           <Link to="/profile">
             {currentUser ? (
@@ -67,7 +72,7 @@ function Header() {
                 alt="profile"
               />
             ) : (
-              <li className=" text-slate-700 hover:underline"> Sign in</li>
+              <li className="  hover:underline"> SIGN IN</li>
             )}
           </Link>
         </ul>
