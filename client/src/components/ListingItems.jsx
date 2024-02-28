31e8bdbbc178529727245e0fa998243470b60b0b
vinaybh01 <vinaybh0107@gmail.com>
2024-02-28 15:06:28 +0530

    changed listing styled

--- client/src/components/ListingItems.jsx
+++ client/src/components/ListingItems.jsx
@@ -3,43 +3,43 @@ import { MdLocationOn } from "react-icons/md";
 
 export default function ListingItems({ list }) {
   return (
-    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden rounded-lg w-full sm:w-[330px]">
-      <Link to={`/list/${list._id}`}>
+    <div className="bg-white shadow-md hover:shadow-lg transition-shadow overflow-hidden  w-full sm:w-[300px] border border-gray-400">
+      <Link to={`/listing/${list._id}`}>
         <img
           src={list.imageUrls[0]}
           alt="list cover"
           className="h-[320px] sm:h-[220px] w-full object-cover hover:scale-105 transition-scale duration-300"
         />
-        <div className="p-3 flex flex-col gap-2 w-full">
-          <p className="truncate text-lg font-semibold text-slate-700">
-            {list.name}
+        <p className="truncate text-[15px] py-0.5 text-center text-white bg-[#02174A] box-border">
+          {list.name}
+        </p>
+        <div className="p-3 flex flex-col gap-2 w-full hover:bg-[#E8EFFD]">
+          <p className="text-black text-[23px] mt-2 font-semibold ">
+            $
+            {list.offer
+              ? (list.regularPrice - list.discountPrice).toLocaleString("en-US")
+              : list.regularPrice.toLocaleString("en-US")}
+            {list.type === "rent" && " / month"}
           </p>
           <div className="flex items-center gap-1">
             <MdLocationOn className="h-4 w-4 text-green-700" />
-            <p className="text-sm text-gray-600 truncate w-full">
+            <p className="text-sm font-semibold text-gray-800 truncate w-full">
               {list.address}
             </p>
           </div>
           <p className="text-sm text-gray-600 line-clamp-2">
             {list.description}
           </p>
-          <p className="text-slate-500 mt-2 font-semibold ">
-            $
-            {list.offer
-              ? list.discountPrice.toLocaleString("en-US")
-              : list.regularPrice.toLocaleString("en-US")}
-            {list.type === "rent" && " / month"}
-          </p>
-          <div className="text-slate-700 flex gap-4">
+          <div className="text-slate-700 flex gap-4 p-2">
             <div className="font-bold text-xs">
               {list.bedrooms > 1
-                ? `${list.bedrooms} beds `
-                : `${list.bedrooms} bed `}
+                ? `${list.bedrooms} BEDS `
+                : `${list.bedrooms} BED `}
             </div>
             <div className="font-bold text-xs">
               {list.bathrooms > 1
-                ? `${list.bathrooms} baths `
-                : `${list.bathrooms} bath `}
+                ? `${list.bathrooms} BATHS `
+                : `${list.bathrooms} BATH `}
             </div>
           </div>
         </div>
